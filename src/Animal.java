3abdf5af833ee2c38ff781577e31d4c478f5f4db
14SR <2802863136@qq.com>
2019-08-22 17:14:45 +0800

    第二章第5题

--- src/Animal.java
+++ src/Animal.java
@@ -1,11 +1,59 @@
+//动物类
 public abstract class Animal {
     private int age;
     public abstract int info(int age);
-
-   /* public Animal(int age){
-        this.age=age;
-    }*/
     public int getAge() {
         return age;
     }
 }
+//鸟类
+class Bird extends Animal {
+    private String color;
+
+    public Bird(String color){
+        this.setColor(color);
+    }
+    public int info(int age){
+        System.out.println("我是一只"+ this.getColor() +"的鸟！"+"\n今年"+age+"岁了！\n");
+        return age;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+}
+//鱼类
+class Fish extends Animal {
+    private int weight;
+
+    public Fish(int weight){
+//        super(age);
+        this.setWeight(weight);
+    }
+    public int info(int age){
+        System.out.println("我是一只"+ this.getWeight() +"几斤重的鱼！"+"\n今年"+age+"岁了！\n");
+        return age;
+    }
+
+    public int getWeight() {
+        return weight;
+    }
+
+    public void setWeight(int weight) {
+        this.weight = weight;
+    }
+}
+//测试类
+class TestAnimal {
+    public static void main(String[] args) {
+        Bird bird=new Bird("红色的");
+        bird.info(4);
+        Fish fish=new Fish(5);
+        fish.info(2);
+    }
+}
+
