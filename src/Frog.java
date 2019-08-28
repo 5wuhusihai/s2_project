432f6e276e5fd5500d2bbb451e0d83aa972dfee0
5wuhusihai <54300281+5wuhusihai@users.noreply.github.com>
2019-08-28 10:23:06 +0800

    Delete Frog.java

--- src/Frog.java
+++
@@ -1,8 +0,0 @@
-public  class Frog extends Animals {
-    public void  pzdw(){
-        System.out.println("锁定目标，用舌头捕捉猎物");
-    }
-    public void say(){
-        System.out.println("呱呱");
-    }
-}
